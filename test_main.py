7ef3a63d788f784bedf20c7d70525a489823b2d6
vutrngkien <vutrungkien2k@gmail.com>
2023-09-01 16:00:24 +0700

    wwwwww

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
-LOG_LEVEL = 'INFO'
+LOG_LEVEL = 'DEBUG'
 
 @pytest.fixture
 def client():
