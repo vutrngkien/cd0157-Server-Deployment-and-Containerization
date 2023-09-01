db53e865ae14857a2fb26a4c9818ea1ea4377d3b
vutrngkien <vutrungkien2k@gmail.com>
2023-09-01 15:55:26 +0700

    ddddddd

--- test_main.py
+++ test_main.py
@@ -11,10 +11,12 @@ SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
+LOG_LEVEL = 'INFO'
 
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
+    os.environ['LOG_LEVEL'] = LOG_LEVEL
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
